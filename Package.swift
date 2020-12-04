db29069ac237ac5e24e12a776cf2f42eeb2db010
consuelita <adelita@operatorfoundation.org>
2020-12-04 17:44:13 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "ReplicantSwiftServerCore", targets: ["ReplicantSwiftServerCore"]),
-        .library(name: "TunObjC", targets: ["TunObjC"])
+        .library(name: "TunObjC", targets: ["TunObjC"]),
         .executable(name: "ReplicantSwiftServer", targets: ["ReplicantSwiftServer"])
     ],
     dependencies: [
