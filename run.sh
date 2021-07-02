de372b526811331bd01ae3aa6c48187a39eb178a
CryptoSax <joshua@operatorfoundation.org>
2021-07-02 15:01:31 -0500

    merged run and build.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-./build.sh
+swift package update || exit 1
+swift build -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx11.0" || exit 2
+
 sudo ./.build/x86_64-unknown-linux-gnu/debug/ReplicantSwiftServer run replicant_server_config.json server_config.json
+
+exit 0
