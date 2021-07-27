dc84e9a5a9f251f2daf7ac37a8f98824e323554a
consuelita <adelita@operatorfoundation.org>
2021-07-27 15:47:21 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 
 import PackageDescription
 
+#if os(macOS)
 let package = Package(
     name: "ReplicantSwiftServer",
     platforms: [
@@ -40,8 +41,57 @@ let package = Package(
 		"InternetProtocols",
 		"Routing",
         "SwiftHexTools",
-                "TransmissionTransport",
-               .product(name: "TransmissionLinux", package: "TransmissionLinux", condition: .when(platforms: [.linux]))
+        "TransmissionTransport",
+        ]),
+        .target(
+            name: "ReplicantSwiftServer",
+            dependencies: ["ReplicantSwiftServerCore"]),
+        .testTarget(
+            name: "ReplicantSwiftServerTests",
+            dependencies: ["ReplicantSwiftServerCore"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
+#else
+let package = Package(
+    name: "ReplicantSwiftServer",
+    platforms: [
+        .macOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(name: "ReplicantSwiftServerCore", targets: ["ReplicantSwiftServerCore"]),
+        .executable(name: "ReplicantSwiftServer", targets: ["ReplicantSwiftServer"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/OperatorFoundation/Flow.git", from: "0.3.0"),
+        .package(url: "https://github.com/OperatorFoundation/Flower.git", from: "0.1.12"),
+        .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from:"1.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.5"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionLinux.git", from: "0.3.2"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionTransport.git", from: "0.2.4"),
+        .package(url: "https://github.com/OperatorFoundation/Tun.git", from: "0.0.10"),
+        .package(url: "https://github.com/OperatorFoundation/ReplicantSwift.git", from: "0.8.6"),
+        .package(url: "https://github.com/OperatorFoundation/Routing.git", from:"0.0.7"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from:"1.2.3"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+
+        .target(
+            name: "ReplicantSwiftServerCore",
+            dependencies: [
+        "ReplicantSwift",
+        "Transport",
+        "Flower",
+        "Tun",
+        "Flow",
+        "InternetProtocols",
+        "Routing",
+        "SwiftHexTools",
+        "TransmissionTransport",
+        .product(name: "TransmissionLinux", package: "TransmissionLinux")
             ]
         ),
         .target(
@@ -53,3 +103,4 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
+#endif
