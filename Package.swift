5700213426c30fcd82675ae426491784675ceb3b
CryptoSax <joshua@operatorfoundation.org>
2021-09-20 13:40:11 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/ReplicantSwift.git", from: "0.10.3"),
         .package(url: "https://github.com/OperatorFoundation/Routing.git", from:"0.0.9"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from:"1.2.3"),
+        .package(url: "https://github.com/OperatorFoundation/Gardener.git", from:"0.0.47"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
