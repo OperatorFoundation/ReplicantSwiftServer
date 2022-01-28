6d2ad5ccac72e370d69aa8d6ba799bb9f0aab21c
CryptoSax <joshua@operatorfoundation.org>
2022-01-28 15:31:48 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 swift package update || exit 1
 swift build || exit 2
 
-sudo ./.build/x86_64-unknown-linux-gnu/debug/ReplicantSwiftServer run replicant_server_config.json server_config.json
+sudo ./.build/x86_64-unknown-linux-gnu/debug/ReplicantSwiftServer run replicant_server_config.json
 
 exit 0
